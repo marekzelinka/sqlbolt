e9a11c523fdb714c72ec4d1cc41c07cd15df7e37
Marek Zelinka <mzelinka17@gmail.com>
2025-11-01 14:28:44 +0100

    format

--- solutions/lesson-7.sql
+++ solutions/lesson-7.sql
@@ -7,5 +7,5 @@ SELECT * FROM buildings;
 -- List all buildings and the distinct employee roles in each building (including empty buildings):
 SELECT DISTINCT role, building_name
 FROM buildings
-LEFT JOIN employees
-    ON buildings.building_name = employees.building;
+    LEFT JOIN employees
+        ON buildings.building_name = employees.building;
